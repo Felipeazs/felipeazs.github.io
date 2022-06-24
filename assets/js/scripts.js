7f4d3f2ae2e8bda91c82e324b3547ee9ce4ef9c7
felipeazs <felipeazs@gmail.com>
2022-06-24 12:45:17 -0400

    cambios mayores

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $('.exp').on('mouseover', function () {
+        $(this).find('p').css('color', '#fff');
+        $(this).find('img').animate({ 'width': '100px' }, 150);
+    })
+
+    $('.exp').on('mouseleave', function () {
+        $(this).find('p').css('color', '#28292d');
+        $(this).find('img').animate({ 'width': '80px' }, 150);
+    })
+
+    //tooltips
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    
+
+
+});
