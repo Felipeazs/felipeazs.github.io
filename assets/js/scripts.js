16612ce9cabfcb056c126288549836c21af94a12
felipeazs <felipeazs@gmail.com>
2022-06-26 14:24:14 -0400

    web counter

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
 
+    let counterContainer = document.querySelector(".website-counter");
+    let visitCount = localStorage.getItem("page_view");
+
+    if (visitCount) {
+        visitCount = Number(visitCount) + 1;
+        localStorage.setItem("page_view", visitCount);
+    } else {
+        visitCount = 1;
+        localStorage.setItem("page_view", 1);
+    }
+    counterContainer.innerHTML = visitCount;
+
     $('.exp').on('mouseover', function () {
         $(this).find('p').css('color', '#E7E7E7');
         $(this).find('img').animate({ 'width': '100px' }, 150);
